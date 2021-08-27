139aa6cdf1082a35375ef6c40412916dffd376d7
ChronoXplorer <57399759+ChronoXplorer@users.noreply.github.com>
2021-08-27 19:43:22 +0700

    Load The Next Scene / Level
    - Next scene are loaded after all of the blocks on that scene / level are destroyed.

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -4,10 +4,28 @@ using UnityEngine;
 
 public class LevelScript : MonoBehaviour
 {
+    // parameters
     [SerializeField] int breakableBlocks; // Serialized for the purpose of debugging
+
+    // cached references
+    SceneLoader sceneloader;
+
+    public void Start()
+    {
+        sceneloader = FindObjectOfType<SceneLoader>();
+    }
 
     public void CountBreakableBlocks()
     {
         breakableBlocks++;
+    }
+
+    public void BlockDestroyed()
+    {
+        breakableBlocks--;
+        if (breakableBlocks <= 0)
+        {
+            sceneloader.LoadNextScene();
+        }
     }
 }
